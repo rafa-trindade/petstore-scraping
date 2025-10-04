354b0cca27ee1df911075c2f1de6656d5472b8e8
Rafael Trindade <rafatrindade.exe@gmail.com>
2025-10-04 11:05:43 -0300

    Initial commit

--- main.py
+++ main.py
@@ -9,52 +9,52 @@ from transform.data_processing import tratarmento_petz
 from transform.data_processing import tratarmento_petlove
 from transform.data_processing import tratarmento_petland
 
-RAW_DIR = os.path.join("data", "raw")
-PROCESSED_DIR = os.path.join("data", "processed")
-os.makedirs(RAW_DIR, exist_ok=True)
-os.makedirs(PROCESSED_DIR, exist_ok=True)
+BRONZE_DIR = os.path.join("data", "bronze")
+SILVER_DIR = os.path.join("data", "silver")
+os.makedirs(BRONZE_DIR, exist_ok=True)
+os.makedirs(SILVER_DIR, exist_ok=True)
 
 def main():
     url = "https://www.petz.com.br/nossas-lojas"
     print("Coletando dados da Petz...")
     df_petz = scrape_petz(url)
-    df_petz.to_csv(os.path.join(RAW_DIR, "petz_raw.csv"), index=False, encoding="utf-8-sig", sep=";")
-    print(f"{len(df_petz)} lojas salvas em data/raw/petz_raw.csv")
+    df_petz.to_csv(os.path.join(BRONZE_DIR, "petz_bronze.csv"), index=False, encoding="utf-8-sig", sep=";")
+    print(f"{len(df_petz)} lojas salvas em data/bronze/petz_bronze.csv")
 
     url = "https://www.petlove.com.br/lojas-fisicas"
     print("Coletando dados da Petlove...")
     df_petlove = scrape_petlove(url)
-    df_petlove.to_csv(os.path.join(RAW_DIR, "petlove_raw.csv"), index=False, encoding="utf-8-sig", sep=";")
-    print(f"{len(df_petlove)} lojas salvas em data/raw/petlove_raw.csv")
+    df_petlove.to_csv(os.path.join(BRONZE_DIR, "petlove_bronze.csv"), index=False, encoding="utf-8-sig", sep=";")
+    print(f"{len(df_petlove)} lojas salvas em data/bronze/petlove_bronze.csv")
     
     url = "https://petlandbrasil.com.br/lojas/"
     print("Coletando dados da Petland...")
     df_petland = scrape_petland(url)
-    df_petland.to_csv(os.path.join(RAW_DIR, "petland_raw.csv"), index=False, encoding="utf-8-sig", sep=";")
-    print(f"{len(df_petland)} lojas salvas em data/raw/petland_raw.csv")
+    df_petland.to_csv(os.path.join(BRONZE_DIR, "petland_bronze.csv"), index=False, encoding="utf-8-sig", sep=";")
+    print(f"{len(df_petland)} lojas salvas em data/bronze/petland_bronze.csv")
     
     # --------------- Transformação ------------------
 
     # Petz
     print("Transformando dados da Petz...")
-    df_petz_processed = tratarmento_petz(os.path.join(RAW_DIR, "petz_raw.csv"))
-    df_petz_processed.to_csv(os.path.join(PROCESSED_DIR, "petz_processed.csv"),
+    df_petz_silver = tratarmento_petz(os.path.join(BRONZE_DIR, "petz_bronze.csv"))
+    df_petz_silver.to_csv(os.path.join(SILVER_DIR, "petz_silver.csv"),
                             index=False, encoding="utf-8-sig", sep=";")
-    print(f"{len(df_petz_processed)} lojas salvas em data/processed/petz_processed.csv")
+    print(f"{len(df_petz_silver)} lojas salvas em data/silver/petz_silver.csv")
 
     # Petlove
     print("Transformando dados da Petlove...")
-    df_petlove_processed = tratarmento_petlove(os.path.join(RAW_DIR, "petlove_raw.csv"))
-    df_petlove_processed.to_csv(os.path.join(PROCESSED_DIR, "petlove_processed.csv"),
+    df_petlove_silver = tratarmento_petlove(os.path.join(BRONZE_DIR, "petlove_bronze.csv"))
+    df_petlove_silver.to_csv(os.path.join(SILVER_DIR, "petlove_silver.csv"),
                                 index=False, encoding="utf-8-sig", sep=";")
-    print(f"{len(df_petlove_processed)} lojas salvas em data/processed/petlove_processed.csv")
+    print(f"{len(df_petlove_silver)} lojas salvas em data/silver/petlove_silver.csv")
 
     # Petland
     print("Transformando dados da Petland...")
-    df_petland_processed = tratarmento_petland(os.path.join(RAW_DIR, "petland_raw.csv"))
-    df_petland_processed.to_csv(os.path.join(PROCESSED_DIR, "petland_processed.csv"),
+    df_petland_silver = tratarmento_petland(os.path.join(BRONZE_DIR, "petland_bronze.csv"))
+    df_petland_silver.to_csv(os.path.join(SILVER_DIR, "petland_silver.csv"),
                                 index=False, encoding="utf-8-sig", sep=";")
-    print(f"{len(df_petland_processed)} lojas salvas em data/processed/petland_processed.csv")
+    print(f"{len(df_petland_silver)} lojas salvas em data/silver/petland_silver.csv")
 
 
 
--- transform/data_processing.py
+++ transform/data_processing.py
@@ -4,7 +4,7 @@ from transform.utils import desmembrar_endereco_petlove, desmembrar_endereco_pet
 def tratarmento_petz(csv_path):
 
   df_petz = pd.read_csv(csv_path, sep=";", encoding="utf-8-sig")
-  df_petz["endereco"] = df_petz["endereco"].str.replace("\n", " ", regex=False).str.replace(r"\s+", " ", regex=True).str.strip()
+  df_petz["endereco"] = df_petz["endereco"].str.replace(r"[\d,\-\n]+", " ", regex=True).str.replace(r"\s+", " ", regex=True).str.strip()
   df_petz["telefone"] = df_petz["telefone"].str.replace("\n", " ", regex=False).str.replace(r"\s+", " ", regex=True).str.strip()
   
   return df_petz
@@ -12,9 +12,9 @@ def tratarmento_petz(csv_path):
 def tratarmento_petlove(csv_path):
 
     df_petlove = pd.read_csv(csv_path, sep=";", encoding="utf-8-sig")
-    df_petlove["endereco"] = df_petlove["endereco"].str.replace("\n", " ", regex=False).str.replace(r"\s+", " ", regex=True).str.strip()
     df_petlove["telefone"] = df_petlove["telefone"].str.replace("\n", " ", regex=False).str.replace(r"\s+", " ", regex=True).str.strip()
     df_petlove[["endereco_desmembrado", "bairro", "cidade", "estado", "cep"]] = df_petlove["endereco"].apply(desmembrar_endereco_petlove)
+    df_petlove["endereco_desmembrado"] = df_petlove["endereco_desmembrado"].str.replace(r"[\d,-]+", " ", regex=True).str.replace(r"\s+", " ", regex=True).str.replace("\n", " ", regex=False).str.strip()
     df_petlove = df_petlove.drop(columns=["endereco"])
     df_petlove = df_petlove.rename(columns={"endereco_desmembrado": "endereco"})
     
@@ -23,9 +23,9 @@ def tratarmento_petlove(csv_path):
 def tratarmento_petland(csv_path):
 
   df_petland = pd.read_csv(csv_path, sep=";", encoding="utf-8-sig")
-  df_petland["endereco"] = df_petland["endereco"].str.replace("\n", " ", regex=False).str.replace(r"\s+", " ", regex=True).str.strip()
   df_petland["telefone"] = df_petland["telefone"].str.replace("\n", " ", regex=False).str.replace(r"\s+", " ", regex=True).str.strip()
   df_petland[["endereco_desmembrado", "bairro", "cidade", "estado", "cep"]] = df_petland["endereco"].apply(desmembrar_endereco_petland)
+  df_petland["endereco_desmembrado"] = df_petland["endereco_desmembrado"].str.replace(r"[\d,-]+", " ", regex=True).str.replace(r"\s+", " ", regex=True).str.replace("\n", " ", regex=False).str.strip()
   df_petland = df_petland.drop(columns=["endereco"])
   df_petland = df_petland.rename(columns={"endereco_desmembrado": "endereco"})
 
