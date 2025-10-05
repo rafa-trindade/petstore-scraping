8c9300d0fd2fdc947414820f0b9c9f4037b96637
Rafael Trindade <rafatrindade.exe@gmail.com>
2025-10-04 23:13:10 -0300

    Initial commit

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import pandas as pd
 from extract.petz_scraper import scrape_petz
 from extract.cobasi_scraper import scrape_cobasi
 from extract.petlove_scraper import scrape_petlove
-from extract.petland_scraper import scrape_petland
 
 BRONZE_DIR = os.path.join("data", "bronze")
 os.makedirs(BRONZE_DIR, exist_ok=True)
@@ -28,13 +27,7 @@ def main():
     df_petlove.to_csv(os.path.join(BRONZE_DIR, "petlove_bronze.csv"), index=False, encoding="utf-8-sig", sep=";")
     print(f"{len(df_petlove)} lojas salvas em data/bronze/petlove_bronze.csv")
     
-    url = "https://petlandbrasil.com.br/lojas/"
-    print("Coletando dados da Petland...")
-    df_petland = scrape_petland(url)
-    df_petland.to_csv(os.path.join(BRONZE_DIR, "petland_bronze.csv"), index=False, encoding="utf-8-sig", sep=";")
-    print(f"{len(df_petland)} lojas salvas em data/bronze/petland_bronze.csv")
-    
-    df_final = pd.concat([df_petz, df_cobasi, df_petlove, df_petland], ignore_index=True)
+    df_final = pd.concat([df_petz, df_cobasi, df_petlove], ignore_index=True)
     df_final.to_csv("data/bronze/lojas_bronze.csv", index=False, sep=";", encoding="utf-8-sig")
     print(f"{len(df_final)} lojas salvas em data/bronze/lojas_bronze.csv")
 
